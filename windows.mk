b31c98194668c2a908349166b62dca9c98ed7dc0
Klas Lindfors <klas@yubico.com>
2014-09-10 16:00:32 +0200

    try to set CC for windows libzip

--- windows.mk
+++ windows.mk
@@ -44,7 +44,7 @@ doit:
 	wget "http://www.nih.at/libzip/libzip-$(LIBZIP_VERSION).tar.gz" && \
 	tar xfa libzip-$(LIBZIP_VERSION).tar.gz && \
 	cd libzip-$(LIBZIP_VERSION) && \
-	lt_cv_deplibs_check_method=pass_all ./configure --host=$(HOST) --build=x86_64-unknown-linux-gnu --prefix=$(PWD)/tmp$(ARCH)/root --with-zlib=$(PWD)/tmp$(ARCH)/root && \
+	CC=$(HOST)-gcc lt_cv_deplibs_check_method=pass_all ./configure --host=$(HOST) --build=x86_64-unknown-linux-gnu --prefix=$(PWD)/tmp$(ARCH)/root --with-zlib=$(PWD)/tmp$(ARCH)/root && \
 	make install && \
 	rm -f $(PWD)/tmp$(ARCH)/root/bin/zipcmp.exe $(PWD)/tmp$(ARCH)/root/bin/zipmerge.exe $(PWD)/tmp$(ARCH)/root/bin/ziptorrent.exe && \
 	rm -rf $(PWD)/tmp$(ARCH)/root/share/ && \
